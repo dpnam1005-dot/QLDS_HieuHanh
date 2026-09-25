2e81f6d0b9cb851eda390e954b0531441737bf35
dpnam1005-dot <dpnam1005@gmail.com>
2026-09-25 20:44:03 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const SB_URL = 'https://ozpaslchfhcdechmrhlv.supabase.co';
-const SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96cGFzbGNoZmhjZGVjaG1yaGx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxMTQxMDUsImV4cCI6MjA5ODY5MDEwNX0.Ekzyal8ona_CjoBkHV19iaDm20DXqCV4MJanSseZ1lo';
-
 let supabaseClient;
 try {
     if (!window.supabase) {
